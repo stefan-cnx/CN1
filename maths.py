909bd456dd76be54ca2487424214ace016f768f4
stefan-cnx <114651927+stefan-cnx@users.noreply.github.com>
2022-10-10 15:51:28 +0100

    Update maths.py

--- maths.py
+++ maths.py
@@ -1,5 +1,7 @@
 # https://www.w3schools.com/python/gloss_python_arithmetic_operators.asp
 
+# Python Arithmetic Operators: Addition
+# =====================================
 # Assign integer value 5 to variable x
 x = 5
 
@@ -13,5 +15,44 @@ print(x + y)
 print(type(x))
 
 
+# Python Arithmetic Operators: Subtraction
+# ========================================
+x = 3
+y = 5
+
+print(x - y)
 
 
+# Python Arithmetic Operators: Multiplication
+# ===========================================
+x = 5
+y = 3
+
+print(x * y)
+
+
+# Python Arithmetic Operators: Division
+# =====================================
+x = 10
+y = 3
+
+# below will output 3.3333333333333335
+print(x / y)
+
+# below will output 10.0
+print(3.3333333333333335 * 3)
+# below will output 10.0
+print(3.3333333333333334 * 3)
+# below will output 10.0
+print(3.3333333333333333 * 3)
+# below will output 10.000000000000004
+print(3.333333333333335 * 3)
+
+z = x / y
+# below will output 3.3333333333333335
+print(z) 
+# below will output 10.0
+print(z * 3)
+
+# Find and print to the console the data type of z
+print(type(z))
