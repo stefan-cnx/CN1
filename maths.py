1c1a4c1dfcdb0a42f452031079c67133181e4796
stefan-cnx <114651927+stefan-cnx@users.noreply.github.com>
2022-10-10 15:08:55 +0100

    Update maths.py

--- maths.py
+++ maths.py
@@ -1,1 +1,17 @@
 # https://www.w3schools.com/python/gloss_python_arithmetic_operators.asp
+
+# Assign integer value 5 to variable x
+x = 5
+
+# Assign integer value 3 to variable y
+y = 3
+
+# Output the sum of x + y to the console
+print(x + y)
+
+# Find and print to the console the data type of x
+print(type(x))
+
+
+
+
