0463f59e5975ed567ec44341b34047ea60c81cb6
stefan-cnx <114651927+stefan-cnx@users.noreply.github.com>
2022-10-19 15:34:52 +0100

    Create maths-01-arithmetic-operators.py

---
+++ maths-01-arithmetic-operators.py
@@ -0,0 +1,90 @@
+# https://www.w3schools.com/python/gloss_python_arithmetic_operators.asp
+
+# Python Arithmetic Operators: Addition
+# =====================================
+# Assign integer value 5 to variable x
+x = 5
+
+# Assign integer value 3 to variable y
+y = 3
+
+# Output the sum of x + y to the console
+print(x + y)
+
+# Find and print to the console the data type of x
+print(type(x))
+
+
+# Python Arithmetic Operators: Subtraction
+# ========================================
+x = 3
+y = 5
+
+print(x - y)
+
+
+# Python Arithmetic Operators: Multiplication
+# ===========================================
+x = 5
+y = 3
+
+print(x * y)
+
+
+# Python Arithmetic Operators: Division
+# =====================================
+x = 10
+y = 3
+
+# below will output 3.3333333333333335
+print(x / y)
+
+# below will output 10.0
+print(3.3333333333333335 * 3)
+# below will output 10.0
+print(3.3333333333333334 * 3)
+# below will output 10.0
+print(3.3333333333333333 * 3)
+# below will output 10.000000000000004
+print(3.333333333333335 * 3)
+
+z = x / y
+# below will output 3.3333333333333335
+print(z) 
+# below will output 10.0
+print(z * 3)
+
+# Find and print to the console the data type of z
+print(type(z))
+
+
+# Different Data Types
+======================
+# the type() function can be used for all data types
+# below will output <class 'int'>
+print(type(9))
+
+# the type() function can be used for all data types
+# below will output <class 'float'>
+print(type(9.9))
+
+# the type() function can be used for all data types
+# below will output <class 'str'>
+print(type('a string'))
+
+# the type() function can be used for all data types
+# below will output <class 'list'>
+print(type(['cat', 'dog', 'monkey']))
+
+# the type() function can be used for all data types
+# below will output <class 'dict'>
+print(type({'item': 'pen', 'inkcolour': 'blue'}))
+
+# the type() function can be used for all data types
+# below will output <class 'bool'>
+print(type(1 == 0))
+# below will output False
+print(1 == 0)
+# below will output True
+print(1 == 1)
+
