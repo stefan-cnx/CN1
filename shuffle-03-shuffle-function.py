f737a2c6b578ea12fdd7562b055c3d73995a400c
stefan-cnx <114651927+stefan-cnx@users.noreply.github.com>
2022-11-15 13:09:52 +0000

    Update shuffle-03-shuffle-function.py

--- shuffle-03-shuffle-function.py
+++ shuffle-03-shuffle-function.py
@@ -10,3 +10,26 @@ def scramble_word(word):
     print('This is the word after it was shuffled from the list and joined into one string with the join() function:', word)
     return word   
 print(scramble_word('PASTA'))
+
+"""
+THIS IS A SAMPLE OUTPUT OF ABOVE SCRIPT:
+========================================
+
+This is the word that was passed into this function: PASTA
+
+This is the list that was created from the 
+list() function using the 'word' that was passed 
+into this function: ['P', 'A', 'S', 'T', 'A']
+
+This is the list after it was shuffled by the 
+shuffle() function: ['T', 'A', 'P', 'S', 'A']
+
+This is the word after it was shuffled from the 
+list and joined into one string with the 
+join() function: TAPSA
+
+TAPSA
+
+"""
+
+
