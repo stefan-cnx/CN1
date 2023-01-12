9dbfe85cd90163890727e6355b54d570b21e86c0
stefan-cnx <114651927+stefan-cnx@users.noreply.github.com>
2023-01-12 16:20:50 +0000

    Add files via upload

---
+++ 20230112 memory-5.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+::Set the path and file name that we want to save our data in
+set filename=c:\stefan\test.txt
+
+::Obtain the data with Windows Commands
+for /f "tokens=1-3 delims=//" %%a in ('date /t') do (set datestamp=%%a/%%b/%%c)
+for /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set timestamp=%%a:%%b)
+for /f "tokens=*" %%a in ('systeminfo ^| findstr Available ^| findstr Physical') do (set freememory=%%a)
+
+::Write the data to a file
+echo On the %datestamp%at %timestamp%, %freememory%. >> %filename%
+
+::pause
